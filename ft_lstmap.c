8fe02fba6ddcb33fbf7f88c7a80873d28f93c55c
EmirSalmanDev <emirsalman.info@gmail.com>
2026-01-23 23:32:48 +0300

    fix: update lstmap function

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,24 +6,36 @@
 /*   By: esalman <esalman@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 12:47:41 by esalman           #+#    #+#             */
-/*   Updated: 2026/01/22 13:05:32 by esalman          ###   ########.fr       */
+/*   Updated: 2026/01/23 23:28:08 by esalman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// void	ft_lstadd_back(t_list **lst, t_list *new)
+// t_list	*ft_lstnew(void *content)
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*iteri;
+	t_list	*new_node;
+	t_list	*root;
+	void	*p;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	iteri = lst;
-	while (iteri)
+	root = NULL;
+	while (lst)
 	{
-		del(iteri->content);
-		f(iteri->content);
-		iteri = iteri->next;
+		p = f(lst->content);
+		new_node = ft_lstnew(p);
+		if (!new_node)
+		{
+			del(new_node->content);
+			ft_lstclear(&root, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&root, new_node);
+		lst = lst->next;
 	}
-	return (lst);
+	return (root);
 }
